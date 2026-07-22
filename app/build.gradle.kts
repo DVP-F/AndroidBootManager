0d5e7fe6922d48d9b103fc949a6c5650cb922103
DVP-F <carnx@duck.com>
2026-07-23 01:06:37 +0200

    EUGHHH gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,15 @@
+import org.gradle.api.tasks.Exec
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.github.willir.rust.cargo-ndk-android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
     namespace = "com.carnx.bootslot"
     compileSdk = 34
-    
+
     defaultConfig {
         applicationId = "com.carnx.bootslot"
         minSdk = 28
@@ -20,20 +21,21 @@ android {
     buildFeatures {
         compose = true
     }
-    
+
     buildTypes {
         release {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                          "proguard-rules.pro"
+                "proguard-rules.pro"
             )
         }
+
         debug {
             isMinifyEnabled = false
         }
     }
-    
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -42,27 +44,37 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-
-    dependencies {
-        implementation(platform("androidx.compose:compose-bom:2024.09.03"))
-
-        implementation("androidx.activity:activity-compose:1.9.2")
-
-        implementation("androidx.compose.ui:ui")
-        implementation("androidx.compose.ui:ui-tooling-preview")
-        implementation("androidx.compose.material3:material3")
-
-        debugImplementation("androidx.compose.ui:ui-tooling")
-    }
 }
 
-cargoNdk {
-    module = "rust"
-    apiLevel = 29
-    targets = arrayListOf(
-        "arm64",
-        "arm",
-        "x86_64",
-        "x86"
+dependencies {
+    implementation(platform("androidx.compose:compose-bom:2024.09.03"))
+
+    implementation("androidx.activity:activity-compose:1.9.2")
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+}
+
+tasks.register<Exec>("buildRust") {
+    workingDir = file("../rust")
+
+    commandLine(
+        // fucker doesnt wanna work with me here it seems. but all paths are ight and it does have the correct env on my system
+        "cargo",
+        "ndk",
+        "-t", "arm64-v8a",
+        "-t", "armeabi-v7a",
+        "-t", "x86",
+        "-t", "x86_64",
+        "-o", "../app/src/main/jniLibs",
+        "build",
+        "--release"
     )
+}
+
+tasks.named("preBuild") {
+    dependsOn("buildRust")
 }
