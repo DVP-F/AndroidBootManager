0eb20cb8ecd8465f413a1d734c6aa8eafda59dca
DVP-F <carnx@duck.com>
2026-07-27 11:48:23 +0200

    add docs for implementation

--- rust/src/error.rs
+++ rust/src/error.rs
@@ -8,6 +8,7 @@ use thiserror::Error;
 
 pub type Result<T> = std::result::Result<T, BootError>;
 
+#[allow(dead_code)] // rustc complains about PermissionDenied and Unknown. This is not an issue.
 #[derive(Debug, Error)]
 pub enum BootError {
     #[error("invalid slot {slot}, device has {slots} slots")]
