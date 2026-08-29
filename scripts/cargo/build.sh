aaa13037b40b5828bcb44c09cd1eed412c7c6d57
DVP-F <carnx@duck.com>
2026-08-29 21:29:50 +0200

    fixed rsbinder generation

--- scripts/cargo/build.sh
+++ scripts/cargo/build.sh
@@ -32,7 +32,7 @@ cd "$(dirname "$0")/../../rust"
 # cargo install cargo-ndk
 # rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
 
-"$CARGO" ndk -t arm64-v8a -t armeabi-v7a -t x86 -t x86_64 -o ../app/src/main/jniLibs build --release
+"$CARGO" ndk -t arm64-v8a -t armeabi-v7a -t x86 -t x86_64 -o ../app/src/main/jniLibs build --release --features "aidl hidl legacy-ffi"
 
 # and go back to start in case this is run in a standalone shell
 cd "$_STARTDIR"
